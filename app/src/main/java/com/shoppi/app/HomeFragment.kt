81256f5d1ab06f90002415c2ca07937796dea453
chaehunPark <chaehun.dev@gmail.com>
2022-11-01 18:50:46 +0900

    [섹션7] JSON asset 불러오기

--- app/src/main/java/com/shoppi/app/HomeFragment.kt
+++ app/src/main/java/com/shoppi/app/HomeFragment.kt
@@ -1,6 +1,7 @@
 package com.shoppi.app
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,7 +9,7 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 
-class HomeFragment: Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -23,7 +24,11 @@ class HomeFragment: Fragment() {
 
         val button = view.findViewById<Button>(R.id.btn_product_detail)
         button.setOnClickListener {
-           findNavController().navigate(R.id.action_home_to_product_Detail)
+            findNavController().navigate(R.id.action_home_to_product_Detail)
         }
+
+        val assetLoader = AssetLoader()
+        val homeData = assetLoader.getJsonString(requireContext(), "home.json")
+        Log.d("homeData", homeData ?: "")
     }
 }
