dd0bc38c1d600ffda68d070d03f93d30b6d24bad
chaehunPark <chaehun.dev@gmail.com>
2022-11-14 22:42:05 +0900

    [섹션8] Data binding 적용, Exception 해결

--- app/src/main/java/com/shoppi/app/ui/home/HomeBannerAdapter.kt
+++ app/src/main/java/com/shoppi/app/ui/home/HomeBannerAdapter.kt
@@ -13,68 +13,34 @@ import androidx.recyclerview.widget.RecyclerView
 import com.shoppi.app.Banner
 import com.shoppi.app.GlideApp
 import com.shoppi.app.R
+import com.shoppi.app.databinding.ItemHomeBannerBinding
 import java.text.DecimalFormat
 import kotlin.math.roundToInt
 
-class HomeBannerAdapter: ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(
+class HomeBannerAdapter : ListAdapter<Banner, HomeBannerAdapter.HomeBannerViewHolder>(
     BannerDiffCallback()
 ) {
+    private lateinit var binding: ItemHomeBannerBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): HomeBannerViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_home_banner, parent, false)
-        return HomeBannerViewHolder(view)
+        binding = ItemHomeBannerBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return HomeBannerViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: HomeBannerViewHolder, position: Int) {
         holder.bind(getItem(position))
     }
 
-    class HomeBannerViewHolder(view: View): RecyclerView.ViewHolder(view) {
-
-        private val bannerImageView = view.findViewById<ImageView>(R.id.iv_banner_image)
-        private val bannerBadgeTextView = view.findViewById<TextView>(R.id.tv_banner_badge)
-        private val bannerTitleTextView = view.findViewById<TextView>(R.id.tv_banner_title)
-        private val bannerDetailThumbnailImageView = view.findViewById<ImageView>(R.id.iv_banner_detail_thumbnail)
-        private val bannerDetailBrandLabelTextView = view.findViewById<TextView>(R.id.tv_banner_detail_brand_label)
-        private val bannerDetailProductLabelTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_label)
-        private val bannerDetailProductDiscountRateTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_discount_rate)
-        private val bannerDetailProductDiscountPriceTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_discount_price)
-        private val bannerDetailProductPriceTextView = view.findViewById<TextView>(R.id.tv_banner_detail_product_price)
+    class HomeBannerViewHolder(private val binding: ItemHomeBannerBinding) : RecyclerView.ViewHolder(binding.root) {
 
         fun bind(banner: Banner) {
-            loadImage(banner.backgroundImageUrl,bannerImageView )
-            bannerBadgeTextView.text = banner.badge.label
-            bannerBadgeTextView.background = ColorDrawable(Color.parseColor(banner.badge.backgroundColor))
-            bannerTitleTextView.text = banner.label
-            loadImage(banner.productDetail.thumbnailImageUri,bannerDetailThumbnailImageView)
-            bannerDetailBrandLabelTextView.text = banner.productDetail.brandName
-            bannerDetailProductLabelTextView.text = banner.productDetail.label
-            bannerDetailProductDiscountRateTextView.text = "${banner.productDetail.discountRate}%"
-            calculateDiscountAmount(bannerDetailProductDiscountPriceTextView, banner.productDetail.discountRate, banner.productDetail.price )
-            applyPriceFormat(bannerDetailProductPriceTextView,banner.productDetail.price )
+            binding.banner = banner
+            binding.executePendingBindings()
         }
-
-        private fun calculateDiscountAmount(view: TextView, discountRate: Int, price: Int) {
-            val discountPrice = (((100 - discountRate) / 100.0) * price).roundToInt()
-            applyPriceFormat(view, discountPrice)
-        }
-
-        private fun applyPriceFormat(view: TextView, price: Int) {
-            val decimalFormat = DecimalFormat("#,###")
-            view.text = decimalFormat.format(price) + "원"
-        }
-
-        private fun loadImage(urlString: String, imageView: ImageView) {
-            GlideApp.with(itemView)
-                .load(urlString)
-                .into(imageView)
-        }
-
     }
-
 }
 
-class BannerDiffCallback: DiffUtil.ItemCallback<Banner>(){
+class BannerDiffCallback : DiffUtil.ItemCallback<Banner>() {
     override fun areItemsTheSame(oldItem: Banner, newItem: Banner): Boolean {
         return oldItem.productDetail.productId == newItem.productDetail.productId
     }
