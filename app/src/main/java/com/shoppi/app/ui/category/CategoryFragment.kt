c1d74b592548e62e4d2093c1133f3b9a15ed8573
chaehunPark <chaehun.dev@gmail.com>
2022-11-29 21:23:36 +0900

    [섹션9] 실습: 네트워크 통신 구현 with Retrofit, Kotlin Coroutine

--- app/src/main/java/com/shoppi/app/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/shoppi/app/ui/category/CategoryFragment.kt
@@ -1,13 +1,18 @@
-package com.shoppi.app.ui
+package com.shoppi.app.ui.category
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import com.shoppi.app.R
+import com.shoppi.app.ui.common.ViewModelFactory
 
 class CategoryFragment: Fragment() {
+
+    private val viewModel: CategoryViewModel by viewModels {ViewModelFactory(requireContext()) }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,5 +20,12 @@ class CategoryFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_category, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        viewModel.items.observe(viewLifecycleOwner) {
+            Log.d("categoryFragment", "items=$it")
+        }
     }
 }
